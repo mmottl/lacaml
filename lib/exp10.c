2da03bb4514c563204a15e51da943bd62453678d
Markus Mottl <markus.mottl@gmail.com>
2009-05-16 14:54:18 -0400

    Fixed compilation problem

--- lib/exp10.c
+++ lib/exp10.c
@@ -5,5 +5,10 @@
  *   glibc-2.3.5.tar.gz/sysdeps/generic/e_exp10.c
  *     CHANGED BY P. COUSOT FOR MACOSX */
 
+#include <math.h>
+
+#ifndef M_LN10
 #define M_LN10 2.30258509299404568402  /* log_e 10 */
+#endif
+
 double exp10 (double arg) { return exp(M_LN10 * arg); }
