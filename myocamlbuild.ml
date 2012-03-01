d7fb52db0cf3ba5ea8418ce064a87bf8eb92564d
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2012-03-01 09:13:19 +0100

    Bumped version to 6.0.4.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 7ce87e2a073ff60e41a80f863a85a1e3) *)
+(* DO NOT EDIT (digest: 0e2c2ec1f1dbddb990953cefd2037b6a) *)
 module OASISGettext = struct
-# 21 "/Users/mmottl/local/godi312/build/apps/apps-oasis/work/oasis-0.2.0/src/oasis/OASISGettext.ml"
+# 21 "/tmp/buildd/oasis-0.2.0/src/oasis/OASISGettext.ml"
   
   let ns_ str = 
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-# 21 "/Users/mmottl/local/godi312/build/apps/apps-oasis/work/oasis-0.2.0/src/oasis/OASISExpr.ml"
+# 21 "/tmp/buildd/oasis-0.2.0/src/oasis/OASISExpr.ml"
   
   
   
@@ -115,7 +115,7 @@ end
 
 
 module BaseEnvLight = struct
-# 21 "/Users/mmottl/local/godi312/build/apps/apps-oasis/work/oasis-0.2.0/src/base/BaseEnvLight.ml"
+# 21 "/tmp/buildd/oasis-0.2.0/src/base/BaseEnvLight.ml"
   
   module MapString = Map.Make(String)
   
@@ -212,7 +212,7 @@ end
 
 
 module MyOCamlbuildFindlib = struct
-# 21 "/Users/mmottl/local/godi312/build/apps/apps-oasis/work/oasis-0.2.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
+# 21 "/tmp/buildd/oasis-0.2.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
   
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -320,7 +320,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-# 21 "/Users/mmottl/local/godi312/build/apps/apps-oasis/work/oasis-0.2.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 21 "/tmp/buildd/oasis-0.2.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
   
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -335,7 +335,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
   
-# 55 "/Users/mmottl/local/godi312/build/apps/apps-oasis/work/oasis-0.2.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 55 "/tmp/buildd/oasis-0.2.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
   
   type t =
       {
