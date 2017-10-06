79f369a29de4b34c1a267a2811a9fa28ef434da9
Markus Mottl <markus.mottl@gmail.com>
2017-10-06 16:57:30 -0400

    Improved example testing script

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -5,10 +5,11 @@ set -e
 for dir in `find . -mindepth 1 -maxdepth 1 -type d`; do
   cd $dir
   for file in *.ml; do
-    ex=`basename $file .ml`.exe
+    ex=`basename $file .ml`
+    exexec=$ex.exe
     echo TESTING $dir/$ex ==================================================
-    jbuilder build $ex
-    ../../_build/default/examples/$dir/$ex
+    jbuilder build $exexec
+    ../../_build/default/examples/$dir/$exexec
   done
   cd ..
 done
